5d32dce83fca96812c82538dd3704f0f74de13a1
Martin Klinke <info@martinklinke.com>
2024-12-02 22:09:55 +0100

    day 1

---
+++ day/1/Program.cs
@@ -0,0 +1,33 @@
+﻿// https://adventofcode.com/2024/day/1
+// part 1
+var input = File.ReadAllText("input.txt");
+var lines = input.Split('\n');
+var leftList = new List<int>();
+var rightList = new List<int>();
+foreach (var line in lines)
+{
+    var values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    leftList.Add(int.Parse(values[0]));
+    rightList.Add(int.Parse(values[1]));
+}
+leftList.Sort();
+rightList.Sort();
+var sum = 0;
+for (int i = 0; i < leftList.Count; i++) {
+    var left = leftList[i];
+    var right = rightList[i];
+    sum += Math.Abs(left - right);
+}
+
+Console.WriteLine("Total distance: " + sum);
+
+//part 2
+var totalScore = 0;
+for(int i=0; i < leftList.Count; i++) {
+    var left = leftList[i];
+    var occurrences = rightList.Count(n => n == left);
+    var score = left * occurrences;
+    totalScore += score;
+}
+
+Console.WriteLine("Total score: " + totalScore);
