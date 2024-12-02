dddb86b369dbaebe0757f434f9ed237ea853a1a9
Martin Klinke <info@martinklinke.com>
2024-12-02 22:32:56 +0100

    day 2 - part 2

--- day/2/Program.cs
+++ day/2/Program.cs
@@ -6,28 +6,41 @@ var reports = input.Split('\n');
 foreach (var report in reports)
 {
     var reportNumbers = report.Split(' ').Select(int.Parse).ToList();
+    
+    if (IsSafe(reportNumbers))
+    {
+        safeCount++;
+    } else {
+        //part 2
+        for(int i = 0; i < reportNumbers.Count; i++) {
+            var reportCopy = reportNumbers.ToList();
+            reportCopy.RemoveAt(i);
+            if(IsSafe(reportCopy)){
+                safeCount++;
+                break;
+            }
+        }
+    }
+}
+Console.WriteLine("Safe count:" + safeCount);
+
+bool IsSafe(List<int> reportNumbers){
     var initialDiff = reportNumbers[1] - reportNumbers[0];
     if (initialDiff == 0)
     {
-        continue;
+        return false;
     }
-    var notSafe = false;
     for (int i = 1; i < reportNumbers.Count; i++)
     {
         var currentDiff = reportNumbers[i] - reportNumbers[i - 1];
         if (currentDiff < 0 && initialDiff > 0 || currentDiff > 0 && initialDiff < 0)
         {
-            notSafe = true;
-            break;
+            return false;
         }
         if (Math.Abs(currentDiff) < 1 || Math.Abs(currentDiff) > 3)
         {
-            notSafe = true;
+            return false;
         }
     }
-    if (!notSafe)
-    {
-        safeCount++;
-    }
+    return true;
 }
-Console.WriteLine("Safe count:" + safeCount);
