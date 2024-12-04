d00890ceb8c4d4899f1c2352cdecd101cdee611d
Martin Klinke <info@martinklinke.com>
2024-12-04 08:26:05 +0100

    day 3 - part 2

--- day/3/Program.cs
+++ day/3/Program.cs
@@ -11,3 +11,27 @@ foreach (var match in matches)
 }
 
 Console.WriteLine("Total sum: " + sum);
+
+// part 2
+var regex2 = new System.Text.RegularExpressions.Regex(@"(do\(\)|don\'t\(\)|mul\(\d+,\d+\))");
+var matches2 = regex2.Matches(input);
+var sum2 = 0;
+var enabled = true;
+foreach (var match in matches2)
+{
+    if (match.ToString() == "do()")
+    {
+        enabled = true;
+    }
+    else if (match.ToString() == "don't()")
+    {
+        enabled = false;
+    }
+    else if (enabled)
+    {
+        var numbers = match.ToString().Substring(4, match.ToString().Length - 5).Split(',').Select(int.Parse).ToList();
+        sum2 += numbers[0] * numbers[1];
+    }
+}
+
+Console.WriteLine("Total sum2: " + sum2);
